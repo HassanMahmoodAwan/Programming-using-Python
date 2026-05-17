1d04dd5f1dba4c672a1fb0e1ed8e65844f3f208a
Hassan Mahmood <hassanmahmudawan@gmail.com>
2026-05-17 22:09:00 +0500

    Learned Basics of Python

---
+++ 01-Basics/01-basics.py
@@ -0,0 +1,36 @@
+# Single Line Comment
+
+"""
+Multi Line Comment,
+As its a string not assigned to any variable, so python Ignores it.
+
+"""
+
+# ******** Escape Sequences ********
+"""
+\n  ==> Next Line (NewLine).
+\t  ==> One Tab Space.
+\\  ==> single \ in the string.
+\'  ==> single quote in string.
+\"  ==> double quote in string
+"""
+
+
+# ********* Print Statement *********
+print("Hello World!")
+print("Hello, My name is Hassan", end="\n")
+print("Welcome", "to Python", sep=", ", end="\t")
+
+
+
+# ********* Input Statement *********
+input("Enter input b\w 0-10: ")
+number = input("Enter any float number: ")
+
+
+# ********* Data Types **************
+"""
+int() ==> Integer, number without decimal Part example 0, -5, 10
+str() ==> String, any text 
+
+"""
